42b886160b58c1ca57cde09795b3326776624260
moond22 <143232485+moond22@users.noreply.github.com>
2024-06-13 21:53:14 +0530

    ASSESSMENT.sol

--- ASSESSMENT.sol
+++ ASSESSMENT.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.8.18;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.26;
 
 /*
        REQUIREMENTS
@@ -14,7 +15,7 @@ pragma solidity 0.8.18;
        to the amount that is supposed to be burned.
 */
 
-pragma solidity 0.8.18;
+pragma solidity 0.8.26;
 
 contract MyToken {
 
